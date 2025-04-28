a27e5675d8daf500a22b6828b854b29df306b7dc
Fidelis <phyd3lis@yandex.com>
2025-04-28 12:05:32 +0100

    firebase hosting

---
+++ config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBRz4NTBjG7R6lNdNPvzzLdyoz9-L3K_TI",
+    authDomain: "toolupims.firebaseapp.com",
+    projectId: "toolupims",
+    storageBucket: "toolupims.firebasestorage.app",
+    messagingSenderId: "982922215520",
+    appId: "1:982922215520:web:744b694f4b09e1f6d44305"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
